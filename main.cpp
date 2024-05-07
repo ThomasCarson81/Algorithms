761b5df96bf05f6325afa7b4b7a91dbcec3fdf7e
Thomas Carson <thomas.carson2024@gmail.com>
2024-05-07 18:20:22 +0100

    Laid out structure
    Randomise() is yet to be implemented

--- main.cpp
+++ main.cpp
@@ -4,7 +4,31 @@ using std::cout;
 using std::endl;
 using std::getline;
 
+void Randomise(int *arr, int len)
+{
+    
+}
+
+bool CheckSorted(int *arr, int len)
+{
+    bool sorted = true;
+    for (int i = 0; i < len-1; i++)
+        if (arr[i] > arr[i+1])
+            sorted = false;
+    return sorted;
+}
+
+void BogoSort(int *arr, int len)
+{
+    while (!CheckSorted(arr, len))
+        Randomise(arr, len);
+}
+
 int main()
 {
-  return 0;
+    int arr[5] = { 5,3,4,1,2 };
+    cout << "Sorting..." << endl;
+    BogoSort(arr, 10);
+    cout << "Done!" << endl;
+    return 0;
 }
