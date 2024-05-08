d0c9d0cd90a6a899d0147b6b6c18365bde6d213a
Thomas Carson <thomas.carson2024@gmail.com>
2024-05-08 11:17:19 +0100

    Implemented Randomise()
    (Not yet tested)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::getline;
 
+srand(time(NULL));
+
+///@brief Get a random number between the bounds
+///@param min Inclusive lower boundary
+///@param max Exclusive upper boundary
+int RandRange(int min, int max)
+{
+    return (rand() % (max+1)) + min;
+}
+
 void Randomise(int *arr, int len)
 {
-    
+    int j, temp;
+    for (int i = 0; i < len; i++)
+    {
+        j = RandRange(0,len);
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
 
 bool CheckSorted(int *arr, int len)
