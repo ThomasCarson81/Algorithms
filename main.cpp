f3f3cd79cf2bfe12fd60a4e26be480e6d6d65410
Thomas Carson <thomas.carson2024@gmail.com>
2024-05-08 13:40:00 +0100

    Fixed srand placement and tested algorithm

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
+#include <time.h>
 using std::cin;
 using std::cout;
 using std::endl;
 using std::getline;
-
-srand(time(NULL));
 
 ///@brief Get a random number between the bounds
 ///@param min Inclusive lower boundary
 ///@param max Exclusive upper boundary
 int RandRange(int min, int max)
 {
-    return (rand() % (max+1)) + min;
+    return (rand() % (max + 1)) + min;
 }
 
 void Randomise(int *arr, int len)
@@ -21,7 +19,7 @@ void Randomise(int *arr, int len)
     int j, temp;
     for (int i = 0; i < len; i++)
     {
-        j = RandRange(0,len);
+        j = RandRange(0, len - 1);
         temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
@@ -30,23 +28,36 @@ void Randomise(int *arr, int len)
 
 bool CheckSorted(int *arr, int len)
 {
-    for (int i = 0; i < len-1; i++)
-        if (arr[i] > arr[i+1])
+    for (int i = 0; i < len - 1; i++)
+        if (arr[i] > arr[i + 1])
             return false;
     return true;
+}
+
+void PrintArray(int *arr, int len)
+{
+    cout << "Array: ";
+    for (int i = 0; i < len; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
 void BogoSort(int *arr, int len)
 {
     while (!CheckSorted(arr, len))
+    {
         Randomise(arr, len);
+        PrintArray(arr, len);
+    }
 }
 
 int main()
 {
-    int arr[5] = { 5,3,4,1,2 };
+    srand(time(NULL));
+    int arr[5] = {5, 3, 4, 1, 2};
+    PrintArray(arr, 5);
     cout << "Sorting..." << endl;
-    BogoSort(arr, 10);
+    BogoSort(arr, 5);
     cout << "Done!" << endl;
     return 0;
 }
